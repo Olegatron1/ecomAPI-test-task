1fb180474f81496e5cc1c110c5a525dbf5785156
Regazzhez <torvald123123@mail.ru>
2024-10-22 19:10:11 +0300

    Create Controllers & request & resource & service

--- app/Http/Controllers/Api/PaymentMethodController.php
+++ app/Http/Controllers/Api/PaymentMethodController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class CartController extends Controller
+class PaymentMethodController extends Controller
 {
     /**
      * Display a listing of the resource.
